65a551f7fc7245605c11fea69d47e210572002fb
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-09-01 03:28:19 +0200

    init-project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use App\Models\Category;
+
+use Illuminate\Support\Facades\Storage;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Storage::deleteDirectory('public/posts');
+        Storage::makeDirectory('public/posts');
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(100)->create();
+        $this->call(PostSeeder::class);
+
+
+    }
+}
